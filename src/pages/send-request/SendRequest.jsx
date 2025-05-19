0bbd0d8ab317186167dd21be0081daa4616d60ed
mosayed40 <moustafa.2950@gmail.com>
2025-05-20 01:12:15 +0300

    construction sendRequest page

--- src/pages/send-request/SendRequest.jsx
+++ src/pages/send-request/SendRequest.jsx
@@ -1,8 +1,63 @@
 import styles from "./SendRequest.module.css";
+import ComponentTitle from "../../comp/ComponentTitle";
+
 export default function SendRequest() {
   return (
     <div className={styles.sendRequest}>
-      <h1>hello world</h1>
+      <ComponentTitle title1={"تقديم طلب شراكة"} />
+      <p>برجاء ملئ البيانات التالية والضغط على ارسال الطلب</p>
+      <div className={styles.sendRequestContianer}>
+        <UnputName />
+        <UnputPhoneAndEmail />
+        <UnputProject />
+        <button>ارسال الطلب</button>
+      </div>
+
+      <img src="../assets/images/photos/Group 3.svg" className={styles.img} />
+    </div>
+  );
+}
+function UnputName() {
+  return (
+    <div className={styles.UnputName}>
+      <h5>
+        الاسم <span>(مطلوب)</span>
+      </h5>
+      <input type="text" placeholder="اكتب الاسم هنا..." />
+    </div>
+  );
+}
+
+function UnputPhoneAndEmail() {
+  return (
+    <div className={styles.contianrt}>
+      <div className={styles.UnputName}>
+        <h5>
+          رقم الهاتف <span>(مطلوب)</span>
+        </h5>
+        <input type="number" placeholder="ادخل رقم الهاتف..." />
+      </div>
+      <div className={styles.UnputName}>
+        <h5>
+          البريد الالكتروني <span>(مطلوب)</span>
+        </h5>
+        <input type="email" placeholder="ادخل عنوان البريد الالكترونى..." />
+      </div>
+    </div>
+  );
+}
+
+function UnputProject() {
+  return (
+    <div className={styles.UnputName}>
+      <h5>
+        وصف المشروع <span>(مطلوب)</span>
+      </h5>
+      <input
+        type="text"
+        placeholder="اعطنا وصف مبسط عن المشروع..."
+        style={{ height: "120px" }}
+      />
     </div>
   );
 }
