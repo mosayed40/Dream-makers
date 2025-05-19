1ea35b9de9e3f811ceff39458e318155baf928bf
mosayed40 <moustafa.2950@gmail.com>
2025-05-19 05:32:12 +0300

    add prices page

--- src/comp/Home-page/ProjectPricingExamples.jsx
+++ src/comp/Home-page/ProjectPricingExamples.jsx
@@ -1,7 +1,15 @@
+import { NavLink } from "react-router-dom";
 import ComponentTitle from "../ComponentTitle";
 import styles from "./ProjectPricingExamples.module.css";
 
 export default function ProjectPricingExamples() {
+  const data = [
+    "نحن ندير الفريق",
+    "تسليم MVP الأساسي",
+    "CTO بدوام جزئي",
+    "4-5 آلاف دولار و5-10% من الأسهم",
+    "الأسهم التي تم تحديدها بشكل متبادل",
+  ];
   return (
     <div className={styles.projectPricingExamples}>
       <div>
@@ -17,33 +25,25 @@ export default function ProjectPricingExamples() {
         الخطوط العريضة لمختلف, استراتيجيات التنمية التي نستخدمها كأمثلة.
       </p>
       <div className={styles.models}>
-        <MiniatureModel />
+        <MiniatureModel title1={"تطوير"} title2={"أعمال فى مراحل النمو"} />
         <div className={styles.MasterModel}>
           <p>نموذج كامل</p>
           <h5>أعمال تم التحقق منها</h5>
-          <div>
-            <img src="/assets/icons/inputIcon.svg" />
-            <p>نحن ندير الفريق</p>
-          </div>
-          <div>
-            <img src="/assets/icons/inputIcon.svg" />
-            <p>تسليم MVP الأساسي</p>
-          </div>
-          <div>
-            <img src="/assets/icons/inputIcon.svg" />
-            <p>CTO بدوام جزئي</p>
-          </div>
-          <div>
-            <img src="/assets/icons/inputIcon.svg" />
-            <p>4-5 آلاف دولار و5-10% من الأسهم</p>
-          </div>
-          <div>
-            <img src="/assets/icons/inputIcon.svg" />
-            <p> الأسهم التي تم تحديدها بشكل متبادل </p>
-          </div>
-          <button className={styles.button}>قدم الان</button>
+
+          {data.map((data, index) => (
+            <div key={index} className={styles.features}>
+              <img src="/assets/icons/inputIcon.svg" />
+              <p> {data}</p>
+            </div>
+          ))}
+          <NavLink to="/request" className={styles.buttonNav}>
+            <button className={styles.button}>قدم الان</button>
+          </NavLink>
         </div>
-        <MiniatureModel />
+        <MiniatureModel
+          title1={"نموذج مصغر"}
+          title2={"أعمال فى مراحل الانشاء"}
+        />
         <img
           src="/assets/images/photos/Group 3.svg"
           className={styles.backgroundImg}
@@ -53,17 +53,7 @@ export default function ProjectPricingExamples() {
   );
 }
 
-function MiniatureModel() {
-  const datalist = [
-    {
-      title: "نموذج كامل",
-      desc: "أعمال تم التحقق منها",
-    },
-    {
-      title: " تطوير",
-      desc: "أعمال تم التحقق منها",
-    },
-  ];
+function MiniatureModel({ title1, title2 }) {
   const features = [
     "نحن ندير الفريق",
     "تسليم MVP الأساسي",
@@ -73,19 +63,20 @@ function MiniatureModel() {
   ];
   return (
     <div className={styles.MiniatureModel}>
-      {datalist.map((item, index) => (
-        <div key={index} className={styles.miniature}>
-          <p>{item.title}</p>
-          <h5>{item.desc}</h5>
-        </div>
-      ))}
+      <div className={styles.miniature}>
+        <p> {title1}</p>
+        <h5 className={styles.title}> {title2}</h5>
+      </div>
+
       {features.map((feature, index) => (
-        <div className={styles.features} key={index}>
+        <div key={index} className={styles.features}>
           <img src="/assets/icons/inputIcon.svg" />
           <p> {feature}</p>
         </div>
       ))}
-      <button className={styles.button}>قدم الان</button>
+      <NavLink to="/request" className={styles.buttonNav}>
+        <button className={styles.button}>قدم الان</button>
+      </NavLink>
     </div>
   );
 }
--- src/pages/prices-page/Price.jsx
+++ src/pages/prices-page/Price.jsx
@@ -1,13 +1,10 @@
+import ProjectPricingExamples from "../../comp/Home-page/ProjectPricingExamples";
+import styles from "./Price.module.css";
+
 export default function Prices() {
   return (
-    <div className="prices">
-      <h1>test prices page</h1>
-      <div className="prices__container">
-        <h1>الأسعار</h1>
-        <div className="prices__content">
-          <p>تفاصيل الأسعار والخدمات المقدمة.</p>
-        </div>
-      </div>
+    <div>
+      <ProjectPricingExamples />
     </div>
   );
 }
