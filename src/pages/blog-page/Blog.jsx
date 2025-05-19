956108171d0d5d0ade1cf804a93eded4d381cb54
mosayed40 <moustafa.2950@gmail.com>
2025-05-19 18:48:06 +0300

    construction service page and blog page

--- src/pages/blog-page/Blog.jsx
+++ src/pages/blog-page/Blog.jsx
@@ -1,13 +1,80 @@
+import styles from "./Blog.module.css";
+import ComponentTitle from "../../comp/ComponentTitle";
+
 export default function Blog() {
   return (
-    <div className="blog">
-      <h1>test blog page</h1>
-      <div className="blog__container">
-        <h1>المدونه</h1>
-        <div className="blog__content">
-          <p>تفاصيل المدونة والمقالات.</p>
-        </div>
+    <div className={styles.blog}>
+      <ComponentTitle title1={"المدونة"} />
+      <div className={styles.blogContent}>
+        <Search />
+        <Options />
       </div>
+      <LatestArticles />
+    </div>
+  );
+}
+function Options() {
+  return (
+    <div className={styles.select}>
+      <p>عرض الاحدث</p>
+      <button>
+        <img src="../assets/icons/icon/options.svg" />
+      </button>
+    </div>
+  );
+}
+function Search() {
+  return (
+    <div className={styles.search}>
+      <input type="text" placeholder="بحث" />
+      <button>
+        <img src="../assets/icons/icon/search.svg" />
+      </button>
+    </div>
+  );
+}
+function LatestArticles() {
+  const articles = [
+    {
+      img: "assets/images/photos/Article_Image.png",
+      date: "10 يناير 2022",
+      title: "إنشاء ألعاب tic tac toe باستخدام React JS",
+    },
+    {
+      img: "assets/images/photos/Article_Image.png",
+      date: "10 يناير 2022",
+      title: "إنشاء ألعاب tic tac toe باستخدام React JS",
+    },
+    {
+      img: "assets/images/photos/Article_Image.png",
+      date: "10 يناير 2022",
+      title: "إنشاء ألعاب tic tac toe باستخدام React JS",
+    },
+    {
+      img: "assets/images/photos/Article_Image.png",
+      date: "10 يناير 2022",
+      title: "إنشاء ألعاب tic tac toe باستخدام React JS",
+    },
+    {
+      img: "assets/images/photos/Article_Image.png",
+      date: "10 يناير 2022",
+      title: "إنشاء ألعاب tic tac toe باستخدام React JS",
+    },
+    {
+      img: "assets/images/photos/Article_Image.png",
+      date: "10 يناير 2022",
+      title: "إنشاء ألعاب tic tac toe باستخدام React JS",
+    },
+  ];
+  return (
+    <div className={styles.latestArticles}>
+      {articles.map((article, index) => (
+        <div className={styles.container} key={index}>
+          <img src={article.img} />
+          <p>{article.date}</p>
+          <span>{article.title} </span>
+        </div>
+      ))}
     </div>
   );
 }
--- src/pages/services/Services.jsx
+++ src/pages/services/Services.jsx
@@ -1,24 +1,44 @@
+import ComponentTitle from "../../comp/ComponentTitle";
+import styles from "./Services.module.css";
+
 export default function Services() {
   return (
-    <div className="services">
-      <h1>test services page</h1>
-      <div className="services__container">
-        <h1>خدماتنا</h1>
-        <div className="services__content">
-          <div className="services__item">
-            <h2>خدمة 1</h2>
-            <p>وصف الخدمة 1</p>
-          </div>
-          <div className="services__item">
-            <h2>خدمة 2</h2>
-            <p>وصف الخدمة 2</p>
-          </div>
-          <div className="services__item">
-            <h2>خدمة 3</h2>
-            <p>وصف الخدمة 3</p>
-          </div>
+    <div className={styles.services}>
+      <ComponentTitle title1={"خدماتنا"} />
+      <p>نقدم خدمة متكاملة من التصميم الى البرمجة والاستضافة</p>
+      <ServicesCard />
+      <img src="../assets/images/photos/Group 3.svg" className={styles.img} />
+    </div>
+  );
+}
+function ServicesCard() {
+  const dataServices = [
+    {
+      icon: "/assets/icons/services/الهوية البصرية.svg",
+      title: " تصميم الهوية البصرية",
+      desc: "هدفنا هو خلق هوية تعبيرية تعكس قيم ورؤية عملك بشكل جاذب واحترافي. بفضل هذه الهوية البصرية المميزة، ستكون قادرًا على بناء الثقة لدى جمهورك وتعزيز التعرف على علامتك التجارية بسهولة، مما يساهم في نجاح عملك ونموه.",
+    },
+    {
+      icon: "/assets/icons/services/برمجة المواقع.svg",
+      title: " تصميم وبرمجة التطبيقات",
+      desc: "هدفنا هو خلق هوية تعبيرية تعكس قيم ورؤية عملك بشكل جاذب واحترافي. بفضل هذه الهوية البصرية المميزة، ستكون قادرًا على بناء الثقة لدى جمهورك وتعزيز التعرف على علامتك التجارية بسهولة، مما يساهم في نجاح عملك ونموه.",
+    },
+    {
+      icon: "/assets/icons/services/برمجة التطبيقات.svg",
+      title: " تصميم وبرمجة المواقع",
+      desc: "هدفنا هو خلق هوية تعبيرية تعكس قيم ورؤية عملك بشكل جاذب واحترافي. بفضل هذه الهوية البصرية المميزة، ستكون قادرًا على بناء الثقة لدى جمهورك وتعزيز التعرف على علامتك التجارية بسهولة، مما يساهم في نجاح عملك ونموه.",
+    },
+  ];
+
+  return (
+    <div className={styles.servicesCard}>
+      {dataServices.map((service, index) => (
+        <div className={styles.container} key={index}>
+          <img src={service.icon} />
+          <h2>{service.title}</h2>
+          <p>{service.desc}</p>
         </div>
-      </div>
+      ))}
     </div>
   );
 }
